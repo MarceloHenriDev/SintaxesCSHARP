f493b775fa3b9c56c104a4ca94e29972041e58d8
Marcelo Henrique <marcelohenridev@gmail.com>
2023-05-29 15:32:12 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+            // if else mas com sintaxe diferente. expressão condicional ternaria
+            // (condição) ? valor_se_verdadeiro : valor_se_falso
+         
+            Console.WriteLine(desconto);
+
+        }
+    }
+}
